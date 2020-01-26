1e64ea8b950aa67ad1502eaa5b103a18ea6256aa
Steven Mills (RIT Student) <sgm4689@ad.rit.edu>
2020-01-26 17:37:37 -0500

    Added animation to rotation, slowed person during rotation

--- Project_One/Assets/Scripts/RotateMaze.cs
+++ Project_One/Assets/Scripts/RotateMaze.cs
@@ -5,27 +5,47 @@ using UnityEngine;
 public class RotateMaze : MonoBehaviour
 {
     public int size;
-    public GameObject player;
     private int[,] mazeMat;
-
+    private bool rotationActive;
+    private float destination;
     // Start is called before the first frame update
     void Start(){
         mazeMat = new int[size, size];
+        rotationActive = false;
     }
 
     // Update is called once per frame
     void Update(){
         if (Input.GetKeyDown(KeyCode.A)) {
-            Rotate(true);
-            transform.Rotate(transform.forward, 90.0f);
+            if (!rotationActive){
+                rotationActive = true;
+                destination = transform.rotation.eulerAngles.z + 90;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.D)){
-            Rotate(false);
-            transform.Rotate(transform.forward, -90);
+            if (!rotationActive)
+            {
+                rotationActive = true;
+                destination = transform.rotation.eulerAngles.z - 90;
+            }
+        }
+        RotateModel();
+    }
+
+    void RotateModel(){
+        if (rotationActive)
+        {
+            Time.timeScale = 0.25f;
+            if (Quaternion.Angle(transform.rotation, Quaternion.Euler(transform.rotation.x, transform.rotation.y, destination)) > 1e-3)//Compare the two rotations, with a small buffer
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.rotation.x, transform.rotation.y, destination), Time.deltaTime * 250);
+            else{
+                rotationActive = false;
+                Time.timeScale = 1.0f;
+            }
         }
     }
 
-    void Rotate(bool left){
+    void RotateMatrix(bool left){
         if (left)
         {
             for (int x = 0; x < size / 2; x++)
