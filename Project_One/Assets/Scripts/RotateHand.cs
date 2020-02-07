0ebd27ae75cfa46845851fcfec01a4184ef26f05
ctd2658 <ctd2658@g.rit.edu>
2020-02-06 20:37:34 -0500

    Started Speedometer

---
+++ Project_One/Assets/Scripts/RotateHand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateHand : MonoBehaviour {
+
+	[SerializeField] private GameObject player;
+	private UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller;
+	private float speed;
+
+	// Use this for initialization
+	void Start()
+	{
+		controller = player.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		speed = controller.m_WalkSpeed;
+
+		RotateToMouse();
+	}
+
+	public void RotateToMouse()
+    {
+		float angle = speed / 50f * 180;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
