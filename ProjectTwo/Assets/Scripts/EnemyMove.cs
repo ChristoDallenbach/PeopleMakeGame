aff1ba63b9fdcd22dbdbac2335fe63c659128032
ctd2658 <ctd2658@g.rit.edu>
2020-02-17 09:52:47 -0500

    Made the cube move, and trying raycasting

---
+++ ProjectTwo/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed;
+    public Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = new Vector3(1,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 position = this.transform.position;
+
+        if (position.x < -5)
+        {
+            direction = new Vector3(1, 0, 0);
+        }
+        else if (position.x > 5)
+        {
+            direction = new Vector3(-1, 0, 0); ;
+        }
+
+        transform.position = transform.position + direction*Time.deltaTime*moveSpeed;
+    }
+}
---
+++ ProjectTwo/Assets/Scripts/Shoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private Vector3 shootDirection;
+    private Vector3 startPosition;
+    [SerializeField] private GameObject cubert;
+    [SerializeField] private Camera camera;
+    private EnemyMove move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = Input.mousePosition;
+        shootDirection = this.transform.forward;
+        move = cubert.GetComponent<EnemyMove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startPosition = Input.mousePosition;
+        startPosition = camera.ScreenToWorldPoint(startPosition);
+        startPosition.z = -10;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.DrawRay((startPosition), shootDirection, Color.red, 5);
+            if (Physics.Raycast((startPosition), shootDirection))
+            {
+                move.direction = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), 0);
+            }
+        }
+    }
+
+}
