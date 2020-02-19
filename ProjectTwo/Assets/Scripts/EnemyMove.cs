19529246ef6e923dab71bdae07473a6420ee39aa
Steven Mills (RIT Student) <sgm4689@ad.rit.edu>
2020-02-19 09:29:28 -0500

    updated movement for cube

--- ProjectTwo/Assets/Scripts/EnemyMove.cs
+++ ProjectTwo/Assets/Scripts/EnemyMove.cs
@@ -7,11 +7,15 @@ public class EnemyMove : MonoBehaviour
 
     [SerializeField] private float moveSpeed;
     public Vector3 direction;
+    private Vector3 cameraMin, cameraMax;
+
 
     // Start is called before the first frame update
     void Start()
     {
         direction = new Vector3(1,0,0);
+        cameraMin = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 10));
+        cameraMax = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 10));
     }
 
     // Update is called once per frame
@@ -24,14 +28,23 @@ public class EnemyMove : MonoBehaviour
     {
         Vector3 position = this.transform.position;
 
-        if (position.x < -5)
+        if (position.x < cameraMin.x)
         {
-            direction = new Vector3(1, 0, 0);
+            direction = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
         }
-        else if (position.x > 5)
+        else if (position.x > cameraMax.x)
         {
-            direction = new Vector3(-1, 0, 0); ;
+            direction = new Vector3(Random.Range(-1.0f, 0.0f), Random.Range(-1.0f, 1.0f), 0);
         }
+        if (position.y < cameraMin.y)
+        {
+            direction = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(0.0f, 1.0f), 0);
+        }
+        else if (position.y > cameraMax.y)
+        {
+            direction = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 0.0f), 0);
+        }
+        direction = direction.normalized;
 
         transform.position = transform.position + direction*Time.deltaTime*moveSpeed;
     }
