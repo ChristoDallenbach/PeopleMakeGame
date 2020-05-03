564928723dc5c424d585685a6237d843f1c344fe
Steven <sgm4689@g.rit.edu>
2020-05-02 22:51:24 -0400

    Started new enemy

---
+++ ProjectTwo/Assets/Scripts/ThiefEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefEnemy : BaseEnemy
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dying)
+        {
+            Destroy(gameObject, 0.1f);//Destroys the game object after a short delay
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    protected override void Move()
+    {
+        direction = Camera.main.transform.position - transform.position;
+        direction = direction.normalized;
+
+        transform.position = transform.position + direction * Time.deltaTime * moveSpeed;
+    }
+}
